a103ae7430a4c5d6e3515f3de8a59ec46a655b1a
nirikshan2004 <97437022+nirikshan2004@users.noreply.github.com>
2022-09-26 21:27:30 +0530

    import java.util.*; class solution{     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int[] a=new int[n];         int f=0;         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();         }         for(int i=0;i<n;i++)         {             int c=0;             for(int j=0;j<n;j++)             {                 if(a[i]==a[j])                 {                     c++;                 }             }             if(c==a[i])             {                 f++;                 a[i]=0;             }         }         System.out.format("%d",f);     } }

---
+++ value_same_as_count.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class solution{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] a=new int[n];
+        int f=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            int c=0;
+            for(int j=0;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    c++;
+                }
+            }
+            if(c==a[i])
+            {
+                f++;
+                a[i]=0;
+            }
+        }
+        System.out.format("%d",f);
+    }
+}
