cea5b681af9bd899770ce6554a69b4e358269143
nirikshan2004 <97437022+nirikshan2004@users.noreply.github.com>
2022-09-29 09:16:07 +0530

    import java.util.Scanner; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         if(n==2)         {             System.out.println(1);         }         else if(n==1)         {             System.out.println(0);         }         else         {             System.out.println(2);         }     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(n==2)
+        {
+            System.out.println(1);
+        }
+        else if(n==1)
+        {
+            System.out.println(0);
+        }
+        else
+        {
+            System.out.println(2);
+        }
+    }
+}
