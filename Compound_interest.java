74ac869f536d2dec084be1457ea8d2685ff428e6
nirikshan2004 <97437022+nirikshan2004@users.noreply.github.com>
2022-10-11 16:54:16 +0530

    import java.util.Scanner; class Solution {     public static void main(String args[])     {         double p,r,t;         Scanner sc=new Scanner(System.in);         p=sc.nextDouble();         r=sc.nextDouble();         t=sc.nextDouble();         double s,a,i;         s=1+(r/100);         a=Math.pow(s,t);         i=(p*a);         System.out.format("%.2f",i);
        }
    }

---
+++ Compound_interest.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+class Solution
+{
+    public static void main(String args[])
+    {
+        double p,r,t;
+        Scanner sc=new Scanner(System.in);
+        p=sc.nextDouble();
+        r=sc.nextDouble();
+        t=sc.nextDouble();
+        double s,a,i;
+        s=1+(r/100);
+        a=Math.pow(s,t);
+        i=(p*a);
+        System.out.format("%.2f",i);
+        
+    }
+}
